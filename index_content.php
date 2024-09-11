0f0a80a0d42cb0ffc5aea681f2070e22329521e6
JEOS7 <tarta.bermuda123@gmail.com>
2024-09-11 12:22:13 -0600

    Add files via upload

---
+++ index_content.php
@@ -0,0 +1,284 @@
+<?php $siteroot = '/' ?>
+
+
+<section> 
+  <div class="content">
+    <div class="row">
+     <div class="example-animation">
+      <div data-lazy-background="<?php echo $siteroot; ?>img/slide1.jpg">
+        <div class="hidden-sm hidden-xs" data-pos="['-30%', '25%', '40%', '15%']" data-duration="700" data-effect="move">
+          <div class="f2 near-white bg-dark-red b pa3">ONLY HI-TECH PRODUCTS</div>
+        </div>
+        <div class="hidden-sm hidden-xs" data-pos="['56%', '-40%', '50%', '5%']" data-duration="700" data-effect="move">
+          <div class="f2 near-white bg-dark-red b pa3">Negociaciones Internacionales Lema</div>
+        </div>
+        <div class="hidden-sm hidden-xs" data-pos="['23%', '110%', '40%', '60%']" data-duration="700" data-effect="move">
+          <img data-lazy-src="<?php echo $siteroot; ?>img/home-icon-slider-01.png?s=200"/>
+        </div>
+      </div>
+      <div data-lazy-background="<?php echo $siteroot; ?>img/slide2.jpg">
+        <div class="hidden-sm hidden-xs" data-pos="['-30%', '25%', '40%', '25%']" data-duration="700" data-effect="move">
+          <div class="f2 near-white bg-dark-red b pa3">Una empresa 100% Mexicana</div>
+        </div>
+        <div class="hidden-sm hidden-xs" data-pos="['56%', '-40%', '50%', '15%']" data-duration="700" data-effect="move">
+          <div class="f2 near-white bg-dark-red b pa3">fundada en el año de 1990</div>
+        </div>
+        <div class="hidden-sm hidden-xs" data-pos="['23%', '110%', '40%', '52%']" data-duration="700" data-effect="move">
+          <img data-lazy-src="<?php echo $siteroot; ?>img/home-icon-slider-02.png?s=200"/>
+        </div> 
+      </div>
+      <div data-lazy-background="<?php echo $siteroot; ?>img/slide3.jpg">
+        <div class="hidden-sm hidden-xs" data-pos="['-30%', '25%', '40%', '25%']" data-duration="700" data-effect="move">
+          <div class="f2 near-white bg-dark-red b pa3">Somos una empresa que brinda soluciones</div>
+        </div>
+        <div class="hidden-sm hidden-xs" data-pos="['56%', '-40%', '50%', '15%']" data-duration="700" data-effect="move">
+          <div class="f2 near-white bg-dark-red b pa3">y especialidades químicas de alta tecnología</div>
+        </div>
+        <div class="hidden-sm hidden-xs" data-pos="['23%', '110%', '40%', '65%']" data-duration="700" data-effect="move">
+          <img data-lazy-src="<?php echo $siteroot; ?>img/home-icon-slider-03.png?s=200"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+
+
+
+    <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <section>
+       <div class="container mv5">
+
+         <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h2 class="text-center bold md dark-red mt4 mb2"> Industrias en las que tenemos presencia</h2>
+          </div>
+        </div>
+
+        <div class="row">
+         <div id="showcase" class="noselect">
+
+           <div class="cloud9-item">
+            <img  src="<?php echo $siteroot; ?>img/productos/maquillaje-square.jpg" alt="Productos de belleza y cuidado de la salud">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/cuidado_personal.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/elastomeros-square.jpg" alt="Polímeros y Elastomeros">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/polimeros.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/recubrimientos-square.jpg" alt="Recubrimientos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/recubrimientos.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/grasas-square.jpg" alt="Lubricantes y grasas">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/grasas.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/agroquimicos-square.jpg" alt="Productos agroquímicos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/agroquimicos.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/limpueza-square.jpg" alt="Productos agroquímicos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/limpieza.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/oil-gas-square.jpg" alt="Productos agroquímicos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/oil.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/plasticos-square.jpg" alt="Productos agroquímicos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/plasticos.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/varios-square.jpg" alt="Productos agroquímicos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/varios.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/tratamiento-de-aguas-square.jpg" alt="Productos agroquímicos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/tratamiento_de_aguas.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+
+          <div class="cloud9-item">
+            <img src="<?php echo $siteroot; ?>img/productos/asfaltos-square.jpg" alt="Productos agroquímicos">
+            <div class="button_carrousel">
+              <a href="<?php echo $siteroot; ?>includes/asfaltos.php" class="btn btn-lg btn-danger">Ver más</a>
+            </div>
+          </div>
+          
+
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section>
+    <div class="bg-trama-lema">
+      <div class="container">
+        <div class="row mb0">
+          <div class="text-center">
+            <h2 class="bold white pa5 athelas f1">"Nuestro producto es el servicio"</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="content mv6">
+     <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h2 class="text-center bold md dark-red mt4 mb2">NUESTRAS REPRESENTADAS</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="tech-slideshow">
+        <div class="mover-1"></div>
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<section>
+ <div class="content mt6 mb6 patron-fondo">
+  <div class="row">
+    <div class="col-md-8 col-xs-12 col-md-offset-2">
+      <h2 class="featurette-heading">Nuestras representadas:</h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4 col-sm-6 col-xs-12">
+      <ul class="lead">
+       <li class="hover-dark-red grow">NOURYON (AKZO NOBEL SURFACE CHEMISTRY, LLC)</li>
+       <li class="hover-dark-red grow">  BIOCOGENT </li>
+       <li class="hover-dark-red grow">  CANCARB LIMITED </li>
+       <li class="hover-dark-red grow">  CELCHEM LLC </li>
+       <li class="hover-dark-red grow">  CONTINENTAL CARBON COMPANY </li>
+       <li class="hover-dark-red grow">HOFFMANN MINERAL GmbH (Private Limited Company) </li>
+       <li class="hover-dark-red grow">  KENRICH PETROCHEMICALS, INC </li>
+              <li class="hover-dark-red grow">  LANXESS </li>
+              <li class="hover-dark-red grow">  ITALMATCH </li>
+
+       
+       
+     </ul>
+   </div>
+   <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="text-center">
+      <img src="<?php echo $siteroot; ?>img/personaje-centro.jpg">
+    </div>
+  </div>
+  <div class="col-md-4 col-sm-6 col-xs-12">
+    <ul class="lead">
+     <li class="hover-dark-red grow">  KETTLITZ-CHEMIE & Co. KG </li>
+     <li class="hover-dark-red grow">  MOMENTIVE PERFORMANCE MATERIALS </li>
+     <li class="hover-dark-red grow"> SHARON LABORATORIES </li>
+     <li class="hover-dark-red grow">  SONNEBORN, INC </li>
+     <li class="hover-dark-red grow">  THE HALLSTAR COMPANY </li>
+     <li class="hover-dark-red grow">  THE INTERNATIONAL GROUP, INC. </li>
+     </ul>
+ </div>
+</div>
+</div>
+</section>
+
+<section>
+  <div class="container marketing mb6">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h2 class="text-center bold md dark-red mt3 mb5"> Filosofía de servicio</h2>
+      </div>
+
+
+      <div class="col-lg-2 col-md-2 col-md-offset-1 col-sm-6 col-xs-12 featuretes">
+        <div class="text-center">
+          <img src="<?php echo $siteroot; ?>img/001-science.png" alt="Productos de alta tecnología LEMA" width="100" height="100">
+        </div>
+        <h3 class="md bold text-center">Sólo productos de alta tecnología </h3>
+        <p class="text-center showme">Distribuimos y comercializamos productos de empresas líderes en calidad y tecnología</p>
+        <p class="text-center"><!--<a class="btn btn-default " href="#" role="button">View details &raquo;</a>--></p>
+      </div><!-- /.col-lg-4 -->
+
+
+      <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 featuretes">
+        <div class="text-center">
+          <img src="<?php echo $siteroot; ?>img/004-time.png" alt="Entregas de productos químicos LEMA" width="100" height="100">
+        </div>
+        <h3 class="md bold text-center">Entregamos en 48 horas máximo </h3>
+        <p class="text-center showme">Entregas mismo día o 48 horas máximo de productos en inventario</p>
+        <p class="text-center"><!--<a class="btn btn-default" href="#" role="button">View details &raquo;</a>--></p>
+      </div><!-- /.col-lg-4 -->
+
+ <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 featuretes">
+        <div class="text-center">
+          <img src="<?php echo $siteroot; ?>img/iso.jpg" alt="Iso 9001-2015" width="100" height="100">
+        </div>
+        <h3 class="md bold text-center">ISO  9001:2015</h3>
+        <p class="text-center showme">ISO  9001:2015</p>
+        <p class="text-center"><!--<a class="btn btn-default" href="#" role="button">View details &raquo;</a>--></p>
+      </div><!-- /.col-lg-4 -->
+      
+
+      <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 featuretes">
+        <div class="text-center">
+         <img src="<?php echo $siteroot; ?>img/003-badge-with-two-ribbons.png" alt="Garantía de entregas LEMA" width="100" height="100">
+       </div>
+       <h3 class="md bold text-center">Garantía y reemplazo de inmediato </h3>
+       <p class="text-center showme">Sustituímos de inmediato cualquier producto que no cumpla con las especificaciones y requerimiento de nuestros clientes</p>
+       <p class="text-center"><!--<a class="btn btn-default" href="#" role="button">View details &raquo;</a>--></p>
+     </div><!-- /.col-lg-4 -->
+
+     <!-- /.col-lg-4 -->
+     <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 featuretes">
+      <div class="text-center">
+       <img src="<?php echo $siteroot; ?>img/002-transport.png" alt="Distribución de productos químicos LEMA" width="100" height="100">
+     </div>
+     <h3 class="md bold text-center">Controlamos el manejo y distribución </h3>
+     <p class="text-center showme">Nuestro personal y equipo de transporte está capacitado para el traslado y entrega profesional de productos químicos</p>
+     <p class="text-center"><!--<a class="btn btn-default" href="#" role="button">View details &raquo;</a>--></p>
+   </div><!-- /.col-lg-4 -->
+
+ </div><!-- /.row -->
+</div>
+</section>
