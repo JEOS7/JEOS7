b12c67a8070bdf3366ea0ddf98c0381bfb16a128
JEOS7 <tarta.bermuda123@gmail.com>
2024-10-14 10:41:44 -0600

    Add files via upload

---
+++ eng_acerca.php
@@ -0,0 +1,165 @@
+<?php $siteroot = '/' ?>
+
+<?php require 'eng_top_content.php' ?>
+
+<section>
+
+ <!-- START THE FEATURETTES -->
+ <div class="container">
+   <hr class="featurette-divider">
+
+   <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">Corporative <span class="text-muted">presentation</span></h2>
+      <p class="lead">LEMA has its general offices, distribution center and laboratory in the Northern Zone of the Metropolitan Area of Mexico City, from where it serves the entire industry based in the Central Zone of the country, Mexico City, State of Mexico, Puebla, Tlaxcala, Morelos, Hidalgo, Queretaro and Guanajuato, where more than 80% of the country's industrial activity is concentrated.</p>
+
+      <p class="lead">To ensure coverage of areas outside the central zone of the country, there are offices in Guadalajara and a warehouse in Monterrey.
+
+</p>
+
+      <p class="lead">The great success of LEMA, whose sales have never stopped growing during its trajectory of since November 1990, is supported by its staff made up of more than 40 Mexicans committed to provide the best service and meet the expectations of our customers. , as well as in the search to become the most important chemical specialty distribution company in Mexico.
+
+
+</p>
+    </div>
+    <div class="col-md-5">
+      <img class="featurette-image img-responsive center-block" src="<?php echo $siteroot; ?>img/mapa-lema.png" alt="Generic placeholder image">
+    </div>
+  </div>
+</div>
+</section>
+
+<section>
+
+
+  <div class="container">
+    <hr class="featurette-divider">
+
+    <div class="row">
+     <div class="col-lg-4 col-sm-12 col-xs-12">
+      <h3><i class="fa fa-search fa-2x"></i>  Mission </h3>
+      <p>We are a company that provides solutions to different industrial sectors through services and high-tech chemical specialties.</p>
+    </div>
+    <div class="col-lg-4 col-sm-12 col-xs-12">
+      <h3><i class="fa fa-eye fa-2x"></i> View</h3> 
+      <p> To be the leading company in the commercialization of chemical specialties, contributing with the social and labor welfare to keep us in the minds of our clients as their best option. </p>
+    </div>
+    <div class="col-lg-4 col-sm-12 col-xs-12">
+     <h3><i class="fa fa-shield fa-2x"></i> Security policy</h3>
+     <p>
+    It is the company's policy to provide the resources, means and conditions for its employees to carry out their activities in a healthy and safe environment.
+
+
+    </p>
+  </div>
+</div>
+</div>
+</section>
+
+
+<section>
+  <div class="container">
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7 col-md-push-5">
+        <h2 class="featurette-heading">Commitment to <span class="text-muted">quality</span></h2>
+        <p class="lead">We take pride in our commitment to quality, the services we provide and the strengthening of our relationships with our clients and proof of this is the implementation of a Quality Management System based on the international standard ISO 9001: 2015, which It will lead us to continuously improve our processes with customer focus.</p>
+        <p class="lead">Our company achieved ISO 9001: 2015 certification under the international standard in December 2017.</p>
+      </div>
+      <div class="col-md-5 col-md-pull-7">
+        <img class="featurette-image img-responsive center-block" src="<?php echo $siteroot; ?>img/medal-warranty.png" alt="Generic placeholder image">
+      </div>
+    </div>
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading"><i class="fa fa-file-o fa-1x"></i>  Our  <span class="text-muted">politic</span></h2>
+         <p class="lead">We are committed to market, store and distribute chemical specialties that meet the needs and expectations of our clients and represented, promoting:</p>
+<ul>
+  <li>
+The permanent compliance with quality and safety standards.
+</li>
+<li>
+The continuous improvement of the processes of the Quality Management System.
+</li>
+<li>
+Relations with our represented and suppliers, to generate lasting and reliable commercial links.
+</li>
+<li>
+The comprehensive training of all our employees, always promoting responsibility, integrity and respect in our environment.
+</li>
+<li>
+Generate profits that guarantee the growth, competitiveness and permanence of the company within the market.
+</li>
+        </ul>
+
+
+      </div>
+      <div class="col-md-5">
+        <img class="featurette-image img-responsive center-block" width="180" src="<?php echo $siteroot; ?>img/nuestra-politica.png" alt="Generic placeholder image">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+    <div class="row featurette">
+     <div class="col-md-5">
+      <img class="featurette-image img-responsive center-block" width="180" src="<?php echo $siteroot; ?>img/nuestro-objetivo.png" alt="Generic placeholder image">
+    </div>
+
+    <div class="col-md-7">
+      <h2 class="featurette-heading"><i class="fa fa-file-text-o fa-1x"></i> Our
+
+ <span class="text-muted"> objectives</span></h2>
+      <ul>
+          <li> Commitment to maintain certification under the ISO standard.</li>
+
+<li>   Supply in a timely manner the products and / or services required by each of our customers or business partners.</li>
+
+<li>  Implement actions to improve our customer service and the SGC.</li>
+
+<li>   Guarantee the acquisition of external products and services with reliable suppliers.</li>
+
+<li> Improve the competence of the staff.</li>
+
+<li>   Perform operations within the planned budgets.</li>
+
+<li>   Meet projected sales.</li>
+
+      </ul>
+
+
+    </div>
+    
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading"><i class="fa fa-file-o fa-1x"></i>  Our  <span class="text-muted"> Values</span></h2>
+        <a href="<?php echo $siteroot; ?>presentaciones/cleaning/lema_CE.pdf" class="black b">
+          <p class="lead">Visit Values and Principles</p></a>
+
+        <ul>
+          
+          
+        </ul>
+
+
+      </div>
+      <div class="col-md-5">
+        <img class="featurette-image img-responsive center-block" width="180" src="<?php echo $siteroot; ?>img/nuestra-politica.png" alt="Generic placeholder image">
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+
+
+
+</div>
+</div>
+</section>
+
+
+<?php require 'eng_footer.php' ?>
