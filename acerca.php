7ece087a2625968329dc4956f37b21a9f1d76b8f
JEOS7 <tarta.bermuda123@gmail.com>
2024-10-14 08:43:30 -0600

    Add files via upload

---
+++ acerca.php
@@ -0,0 +1,138 @@
+<?php $siteroot = '/' ?>
+
+<?php require 'top_content.php' ?>
+
+<section>
+
+ <!-- START THE FEATURETTES -->
+ <div class="container">
+   <hr class="featurette-divider">
+
+   <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">Presentación <span class="text-muted">Corporativa</span></h2>
+      <p class="lead">LEMA cuenta con sus oficinas generales y almacenes de distribución en la Zona Norte del Área Metropolitana de la Ciudad de México, desde donde da servicio a toda la industria asentada en la Zona Centro del país, Ciudad de México, Estado de México, Puebla, Tlaxcala, Morelos, Hidalgo, Querétaro y Guanajuato donde se concentra la mayor actividad industrial del país y puede asegurar una cobertura de las zonas fuera del centro del país.</p>
+
+      <p class="lead"> </p>
+
+      <p class="lead">El éxito de LEMA se soporta en su personal comprometido con brindar el mejor servicio y satisfacer las expectativas de nuestros clientes, así como en la busqueda de convertirse en la empresa de distribución de especialidades químicas más importante de México.</p>
+    </div>
+    <div class="col-md-5">
+      <img class="featurette-image img-responsive center-block" src="<?php echo $siteroot; ?>img/mapa-lema.png" alt="Generic placeholder image">
+    </div>
+  </div>
+</div>
+</section>
+
+<section>
+
+
+  <div class="container">
+    <hr class="featurette-divider">
+
+    <div class="row">
+     <div class="col-lg-4 col-sm-12 col-xs-12">
+      <h3><i class="fa fa-search fa-2x"></i>  Misión </h3>
+      <p>Somos una empresa que brinda soluciones a los diferentes sectores industriales a través de servicios y especialidades químicas de alta tecnología.</p>
+    </div>
+    <div class="col-lg-4 col-sm-12 col-xs-12">
+      <h3><i class="fa fa-eye fa-2x"></i> Visión</h3> 
+      <p>Ser la empresa líder en la comercialización de especialidades químicas, contribuyendo con el bienestar social y laboral para mantenernos en la mente de nuestros clientes como su mejor opción.</p>
+    </div>
+    <div class="col-lg-4 col-sm-12 col-xs-12">
+     <h3><i class="fa fa-shield fa-2x"></i> Política de seguridad </h3>
+     <p>
+      Es política de la empresa, proporcionar los recursos, medios y condiciones para que sus empleados, realicen sus actividades en un ambiente sano y seguro.
+    </p>
+  </div>
+</div>
+</div>
+</section>
+
+
+<section>
+  <div class="container">
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7 col-md-push-5">
+        <h2 class="featurette-heading">Compromiso con la <span class="text-muted">calidad</span></h2>
+        <p class="lead">Nos enorgullecemos de nuestro compromiso con la calidad, de los servicios que prestamos y el fortalecimiento de las relaciones con nuestros clientes y prueba de ello es la implementación de un Sistema de Gestión de la Calidad con base a la norma internacional ISO 9001:2015, que nos ayuda a mejorar continuamente nuestros procesos con enfoque al cliente.</p>
+        <p class="lead">Nuestra empresa logró la certificación bajo la norma internacional ISO 9001:2015 en Diciembre 2017.</p>
+      </div>
+      <div class="col-md-5 col-md-pull-7">
+        <img class="featurette-image img-responsive center-block" src="<?php echo $siteroot; ?>img/medal-warranty.png" alt="Generic placeholder image">
+      </div>
+    </div>
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading"><i class="fa fa-file-o fa-1x"></i> Nuestra <span class="text-muted">Política de Calidad</span></h2>
+        <p class="lead">Estamos comprometidos a comercializar, almacenar y distribuir especialidades químicas que cumplan con los requisitos aplicables a todas las partes interesadas, promoviendo:</p>
+
+        <ul>
+          <li>El cumplimiento permanente de las normas de calidad y seguridad </li>
+          <li>El mejoramiento continuo de los procesos del Sistema de Gestión de la Calidad </li>
+          <li>Relaciones con nuestras representadas y proveedores, para generar lazos comerciales duraderos y confiables </li>
+          <li>La formación integral de todos nuestros empleados, fomentando siempre la responsabilidad, integridad y el respeto a diario en nuestro entorno </li>
+          <li>Generar utilidades que garanticen el crecimiento, competitividad y permanencia de la empresa dentro del mercado </li>
+        </ul>
+
+
+      </div>
+      <div class="col-md-5">
+        <img class="featurette-image img-responsive center-block" width="180" src="<?php echo $siteroot; ?>img/nuestra-politica.png" alt="Generic placeholder image">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+    <div class="row featurette">
+     <div class="col-md-5">
+      <img class="featurette-image img-responsive center-block" width="180" src="<?php echo $siteroot; ?>img/nuestro-objetivo.png" alt="Generic placeholder image">
+    </div>
+
+    <div class="col-md-7">
+      <h2 class="featurette-heading"><i class="fa fa-file-text-o fa-1x"></i> Nuestros <span class="text-muted">Objetivos</span></h2>
+      <ul>
+        <li>Compromiso de mantener la certificación bajo la norma ISO</li>
+        <li>Suministrar en tiempo y forma los productos y/o servicios requeridos por cada uno de nuestros clientes o socios de negocio</li>
+        <li>Implementar acciones que permitan mejorar nuestro servicio al cliente y al SGC</li>
+        <li>Garantizar la adquisición de productos y servicios externos con proveedores confiables</li>
+        <li>Mejorar la competencia del personal </li>
+        <li>Realizar operaciones dentro de los presupuestos planificados </li>
+        <li>Cumplir con la proyección de venta </li>
+
+      </ul>
+
+
+    </div>
+
+		<hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading"><i class="fa fa-file-o fa-1x"></i> Nuestros <span class="text-muted">Valores</span></h2>
+        <a href="<?php echo $siteroot; ?>presentaciones/cleaning/lema_CE.pdf" class="black b">
+          <p class="lead">Visite nuestro Código de Ética</p></a>
+
+        <ul>
+          
+          
+        </ul>
+
+
+      </div>
+      <div class="col-md-5">
+        <img class="featurette-image img-responsive center-block" width="180" src="<?php echo $siteroot; ?>img/nuestra-politica.png" alt="Generic placeholder image">
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+</div>
+</div>
+</section>
+
+
+<?php require 'footer.php' ?>
