e2ffe48b151dc4840608f97a89395eead74ead17
JEOS7 <tarta.bermuda123@gmail.com>
2024-10-14 12:03:26 -0600

    Add files via upload

---
+++ oil.php
@@ -0,0 +1,76 @@
+<?php $siteroot = '/' ?>
+
+<?php require 'top_content.php' ?>
+
+<section>
+  <div class="container">
+   <div class="row mt6">
+    <div class="col-md-12">
+     <h1 class="underline b">OIL&GAS - PETROLEO Y GAS (PERFORACIÓN, PRODUCCIÓN, ESTIMULACIÓN)</span> </h1>
+   </div>
+ </div>
+ <!-- Producto 1 -->
+ <div class="row">
+   <div class="col-md-7 col-sm-7 col-xs-12">
+    <div class="single-portfolio mv2">
+      <img src="<?php echo $siteroot; ?>img/productos/oil-gas.jpg">
+    </div>
+  </div>
+  <div class="col-md-5 col-sm-5 col-xs-12">
+    <p class="f3 b">CONTAMOS CON PRODUCTOS DE PRIMERA CALIDAD DE FABRICANTES DE RECONOCIMIENTO MUNDIAL.</p>
+    <ul class="f4"> 
+      <li> <span class="b"> NOURYON:</span> WITBREAK, ARMOHIB, ALCOFLOW, WITCONATE, AQUATREAT, AQUAMET, AMADOL,VERSA, ARMOVIS</li>
+
+      <li> <span class="b"> MOMENTIVE:</span> ANTIESPUMANTES SAG.</li>
+
+      <div class="bg-trama-lema">
+        <div class="pa4">
+          <div class="text-center">
+            <h5 class="md bold white">¿Te interesa nuestro catálogo o solicitar carta de presentación? <br/>
+<div style="padding-right: 20px; padding-top: 15px;"><a class="typeform-share button" href="https://contactolema.typeform.com/to/k8aUZv" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#fff;color:#e91919;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:15px;line-height:37.5px;text-align:center;margin:0;height:37.5px;padding:0px 25px;border-radius:18px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank">Contáctanos </a> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
+</div>            </h5>
+            <span itemprop="telephone">
+              <a href="tel:+011525558712600 " class="white b">
+              TEL: (55)5871 2600</a></span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container mb6">
+    <div class="row">
+      <div class="col-md-7">
+        <h3 class="b">PORTAFOLIO DE PRODUCTOS </h3>
+        <ul class="portafolio">
+          <li>EMULSIFICANTES.</li>
+<li>PROMOTORES DE ADHERENCIA.</li>
+<li>EMULSIFICANTES ANIÓNICOS.</li>
+<li>EMULSIFICANTES CATIÓNICOS.</li>
+        </ul>
+      </div>
+
+      <div class="col-md-5">
+       <div class="mv4">
+        <div class="text-center">
+          
+          <img src="<?php echo $siteroot; ?>img/descarga-lema.png" alt="" width="280">
+
+        </div>
+      </div>
+    </div>
+
+
+
+  </div>
+</div>
+<!--  END PRODUCTO 1-->
+</div>
+</div>
+</div>
+</section>
+
+
+<?php require 'footer.php' ?>
